987b54cc2803358b257f9d879d59e6b8f12ead72
Greyflanel <richardlovelace811@gmail.com>
2019-04-01 16:57:40 -0500

    My first commit. Just getting started with the image thumbnail.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+
+const HeaderContainer = () => {
+    return <div>
+<ImageThumbnail />
+    </div>;
+}
+
+export default HeaderContainer;
